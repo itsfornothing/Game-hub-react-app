3c9ecd0212e4d9b71cb101129a3599b94508be49
halid <halidmohamed807@gmail.com>
2026-03-14 16:39:01 +0300

    customize the theme to get darker grays

--- src/theme.ts
+++ src/theme.ts
@@ -2,7 +2,7 @@ import { createSystem, defaultConfig } from "@chakra-ui/react";
 
 const config = {
   ...defaultConfig,
-  initialColorMode: "dark" as const, 
+  initialColorMode: "dark" as const,
   useSystemColorMode: false,
 };
 
@@ -10,6 +10,20 @@ export const system = createSystem(config);
 
 const theme = {
   ...system,
+  colors: {
+    gray: {
+      50: "#f9f9f9",
+      100: "#ededed",
+      200: "#d3d3d3",
+      300: "#b3b3b3",
+      400: "#a0a0a0",
+      500: "#898989",
+      600: " #6c6c6c",
+      700: "#202020",
+      800: "#121212",
+      900: "#111",
+    },
+  },
 };
 
 export default theme;
